cb199aa4e922d3b194bdbe02ba1696d4e4a2bc17
时点软件 冯成林 <Charlin@fengchenglindeiMac.local>
2015-11-28 16:21:13 +0800

    init

---
+++ CoreIV/ViewController.m
@@ -0,0 +1,33 @@
+//
+//  ViewController.m
+//  CoreIV
+//
+//  Created by 冯成林 on 15/11/28.
+//  Copyright © 2015年 冯成林. All rights reserved.
+//
+
+#import "ViewController.h"
+#import "CoreIV.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    
+    [CoreIV showWithType:IVTypeError view:self.view msg:@"无网络" failClickBlock:^{
+        
+        NSLog(@"重新加载");
+    }];
+    
+}
+
+
+
+
+
+@end
